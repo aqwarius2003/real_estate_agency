e1e005e65a68bff6ad5946d294d344af4b6fd696
aqwarius2003 <00victorr@gmail.com>
2024-07-16 13:59:24 +0400

    Customize Flat Admin - read-only created_at

--- property/admin.py
+++ property/admin.py
@@ -5,5 +5,6 @@ from .models import Flat
 class FlatAdmin(admin.ModelAdmin):
     list_display = ('town', 'address', 'owner')
     search_fields = ['town', 'address', 'owner']  # Поля для поиска: город, адрес, имя владельца
+    readonly_fields = ['created_at']
 
 admin.site.register(Flat, FlatAdmin)

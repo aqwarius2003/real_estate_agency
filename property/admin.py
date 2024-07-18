1a858c2b8073a7607e1e40ac50b350a1274162b1
aqwarius2003 <00victorr@gmail.com>
2024-07-18 14:04:50 +0400

    add db model for complaints.

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Complaint
+
+
+class ComplaintInline(admin.TabularInline):
+    model = Complaint
+    extra = 0
+
+class Meta:
+    verbose_name = 'Жалоба'
+    verbose_name_plural = 'Жалобы'
 
 class FlatAdmin(admin.ModelAdmin):
     list_display = ('town', 'address', 'new_building', 'owner')
@@ -8,4 +17,18 @@ class FlatAdmin(admin.ModelAdmin):
     readonly_fields = ['created_at']
     list_editable = ('new_building',)
     list_filter = ('new_building',)
+    inlines = [ComplaintInline]
+
+
 admin.site.register(Flat, FlatAdmin)
+
+
+@admin.register(Complaint)
+class ComplaintAdmin(admin.ModelAdmin):
+    list_display = ('user', 'flat', 'text')
+    raw_id_fields = ('flat',)  # Добавляем raw_id_fields для поля ForeignKey flat
+    verbose_name = 'Жалоба'
+    verbose_name_plural = 'Жалобы'
+
+
+# admin.site.register(Complaint, ComplaintAdmin)
